b92a406ee05073eb54500bb115f171b7c72e0fa3
Aremaki <ad.remaki@gmail.com>
2025-09-22 12:05:05 +0200

    update edsnlp and remove rel extension by default

--- biomedics/ner/__init__.py
+++ biomedics/ner/__init__.py
@@ -2,8 +2,6 @@ import spacy
 
 if not spacy.tokens.Span.has_extension("event_type"): # type: ignore
     spacy.tokens.Span.set_extension("event_type", default=None) # type: ignore
-if not spacy.tokens.Span.has_extension("rel"): # type: ignore
-    spacy.tokens.Span.set_extension("rel", default=None) # type: ignore
 
 for ext in [
     "assertion",
