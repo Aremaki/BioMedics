84b0207528f6378b3b9a75062720301f05632953
Aremaki <ad.remaki@gmail.com>
2025-09-23 10:40:26 +0200

    remove sbtach

--- scripts/patient_similarity/run.sh
+++ scripts/patient_similarity/run.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=patient_similarity
-#SBATCH -t 48:00:00
-#SBATCH --gres=gpu:v100:1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=20000
-#SBATCH --partition gpuV100
-#SBATCH --output=logs/slurm-%j-stdout.log
-#SBATCH --error=logs/slurm-%j-stderr.log
-#SBATCH --container-image /scratch/images/sparkhadoop.sqsh  --container-mounts=/export/home/$USER:/export/home/$USER,/data/scratch/$USER:/data/scratch/$USER --container-mount-home --container-writable
-source $HOME/.user_conda/miniconda/etc/profile.d/conda.sh # appel de ce script
 cd "/export/home/cse200093/Adam/biomedics/scripts/patient_similarity"
 source "/export/home/cse200093/Adam/biomedics/.venv/bin/activate"
 conda deactivate
