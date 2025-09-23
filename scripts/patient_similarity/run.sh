49b171e7105bb037afb297676c6f6b7e0c5f39cb
Aremaki <ad.remaki@gmail.com>
2025-09-23 10:41:37 +0200

    remove conda

--- scripts/patient_similarity/run.sh
+++ scripts/patient_similarity/run.sh
@@ -1,6 +1,5 @@
 cd "/export/home/cse200093/Adam/biomedics/scripts/patient_similarity"
 source "/export/home/cse200093/Adam/biomedics/.venv/bin/activate"
-conda deactivate
 
 echo -----------------
 echo COMPUTE PATIENT SIMILARITY
